cc5718d3e0841f2cfac091bf3a76fba7dafe8a2c
kitono69 <kitako.so@hotmail.com>
2021-10-13 07:58:39 +0000

    more functions

--- csvtest.py
+++ csvtest.py
@@ -1,6 +1,13 @@
 import pandas as pd
 
-pita6 = pd.read_csv("data/6.csv", encoding='latin1')
+pita = pd.read_csv("data/6.csv", encoding='latin1')
+#print (pita.head(5))
 
-print (pita6.tail())
+drop_pita = pita.drop(pita.columns[7],axis=1) #ini hapus kolom file info
+#print (drop_pita.head())
 
+sort_pita = drop_pita.sort_values(by=['Level (dBµV/m)']) #sort kolom level dari yang paling kecil
+#print (sort_pita.head(20)) 
+
+hitung_data = pita.shape[0] #hitung jumlah baris
+print (hitung_data)
